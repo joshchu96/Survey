0780c2816b1baec63130936fb55fb9843c6f85a3
Joshua Chu <131711421+joshchu96@users.noreply.github.com>
2024-04-11 10:25:55 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template, redirect, request, flash
+from flask import Flask,render_template, redirect, request, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey 
 
@@ -7,7 +7,8 @@ app.config['SECRET_KEY']= "very_secret_key"
 
 #create the debugger toolbar
 toolbar= DebugToolbarExtension(app)
-responses =[]
+
+RESPONSES_KEY = "responses"
 
 
 @app.route("/")
@@ -16,10 +17,15 @@ def show_survey_intro():
 
 @app.route("/begin-survey", methods=["POST"])
 def begin_survey():
+
+    session[RESPONSES_KEY] = []
+
     return redirect("/questions/0")
 
 @app.route("/questions/<int:qindex>")
 def show_question(qindex):
+
+    responses = session.get(RESPONSES_KEY)
 
     if (responses) is None:
         return redirect("/")
@@ -37,10 +43,13 @@ def show_question(qindex):
 
 @app.route("/answers", methods=["POST"])
 def handle_answers():
+
    
     choice = request.form['answer']
     #add choice to the list for responses
+    responses= session[RESPONSES_KEY]
     responses.append(choice)
+    session[RESPONSES_KEY] = responses
 
     return redirect(f"/questions/{len(responses)}")
 
